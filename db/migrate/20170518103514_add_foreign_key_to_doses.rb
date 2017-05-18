36c7454fc355604d1632afb2d7446b3855de8e88
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-05-18 12:39:19 +0200

    models tests

---
+++ db/migrate/20170518103514_add_foreign_key_to_doses.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyToDoses < ActiveRecord::Migration[5.0]
+  def change
+    add_foreign_key :doses, :cocktails
+    add_foreign_key :doses, :ingredients
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518093920) do
+ActiveRecord::Schema.define(version: 20170518103514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,6 @@ ActiveRecord::Schema.define(version: 20170518093920) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
