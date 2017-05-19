715d6c2f9412d9ab2cb4b13244722f4e5cbad148
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-05-19 12:24:11 +0200

    image upload

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -47,6 +47,6 @@ class CocktailsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def cocktail_params
-      params.require(:cocktail).permit(:name)
+      params.require(:cocktail).permit(:name, :photo)
     end
 end
