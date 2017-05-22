2fe8c78a932bedd2420e3d6ebfb407b3d7175b77
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-05-22 10:55:24 +0200

    Add missing ingredients in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'jus multifruits')
+Ingredient.create(name: 'jus de mangue')
+Ingredient.create(name: 'jus de pêche')
+Ingredient.create(name: "jus d'orange")
+Ingredient.create(name: "jus de pommes")
+Ingredient.create(name: "jus de fraises")
+Ingredient.create(name: "jus de goyaves")
+Ingredient.create(name: "jus de tomates")
+Ingredient.create(name: "liqueur de caramel")
+Ingredient.create(name: "liqueur de pomme")
+Ingredient.create(name: "jus de cramberries")
+Ingredient.create(name: 'poudre de vanille')
+Ingredient.create(name: 'sucre vanillé')
+Ingredient.create(name: 'cointreau')
 
 require 'json'
 require 'open-uri'
@@ -15,8 +29,9 @@ drinks = JSON.parse(serialized_drinks)
 
 i = 0
 
-40.times do
+60.times do
   ingredient = Ingredient.create(name: drinks['drinks'][i]['strIngredient1'])
   i += 1
   ingredient.save!
 end
+
